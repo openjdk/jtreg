357638b18363fae90c1442a98913e84e58fdb554
Jonathan Gibbons <jjg@openjdk.org>
2013-03-08 10:58:35 -0800

    7900107: jtreg should doc the existence of support for @files

--- src/share/classes/com/sun/javatest/regtest/Help.java
+++ src/share/classes/com/sun/javatest/regtest/Help.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006, 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006, 2013, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -376,6 +376,9 @@ public class Help {
                     nodesForGroup.toArray(new HelpTree.Node[nodesForGroup.size()]));
             commandHelpTree.addNode(groupNode);
         }
+
+        HelpTree.Node n = new HelpTree.Node(i18n, "help.at", new String[] { });
+        commandHelpTree.addNode(n);
 
         String progName = getProgramName();
 
