89bf874ed6fb8a1ce6e4e49b2b8b3f69aaa689c2
Jonathan Gibbons <jjg@openjdk.org>
2015-01-16 19:05:44 -0800

    7901260: Allow test descriptions in .jasm and .jcod files

--- src/share/classes/com/sun/javatest/regtest/RegressionTestFinder.java
+++ src/share/classes/com/sun/javatest/regtest/RegressionTestFinder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -77,6 +77,8 @@ public class RegressionTestFinder extends TagTestFinder
         exclude(excludeNames);
         addExtension(".sh", ShScriptCommentStream.class);
         addExtension(".html", HTMLCommentStream.class);
+        addExtension(".jasm", JavaCommentStream.class);
+        addExtension(".jcod", JavaCommentStream.class);
 
         baseContext = new RegressionContext();
     }
