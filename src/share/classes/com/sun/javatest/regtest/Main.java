3a2d73c71aa7cebdf4b81346d31e7bd9c7155157
Jonathan Gibbons <jjg@openjdk.org>
2016-03-15 13:30:39 -0700

    Explicit support for -Xpatch to ensure path is made absolute

--- src/share/classes/com/sun/javatest/regtest/Main.java
+++ src/share/classes/com/sun/javatest/regtest/Main.java
@@ -582,6 +582,18 @@ public class Main {
             }
         },
 
+        new Option(STD, JDK, null, "Xpatch") {
+            public void process(String opt, String arg) {
+                // old style for now: -Xpatch:<path>
+                testVMOpts.add("-Xpatch:" + filesToAbsolutePath(pathToFiles(arg)));
+//                // new style, to come:  -Xpatch:<module>=<path>
+//                int eq = arg.indexOf("=");
+//                testVMOpts.add("-Xpatch:"
+//                        + arg.substring(0, eq + 1)
+//                        + filesToAbsolutePath(pathToFiles(arg.substring(eq + 1))));
+            }
+        },
+
         new Option(WILDCARD, JDK, null, "X") {
             public void process(String opt, String arg) {
                 // This is a change in spec. Previously. -X was used to tunnel
