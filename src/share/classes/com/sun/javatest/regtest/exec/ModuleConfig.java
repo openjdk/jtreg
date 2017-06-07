2ccdec9e1275ebbb22ac204fde88846543b95595
Jonathan Gibbons <jjg@openjdk.org>
2017-06-07 13:43:30 -0700

    Fix typo in module config reporting

--- src/share/classes/com/sun/javatest/regtest/exec/ModuleConfig.java
+++ src/share/classes/com/sun/javatest/regtest/exec/ModuleConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -149,7 +149,7 @@ public class ModuleConfig {
     ModuleConfig setAddOpensToUnnamed(Set<String> modules) {
         for (String module: modules) {
             if (module.contains("/")) {
-                setAddExports(module, Collections.singletonList("ALL-UNNAMED"));
+                setAddOpens(module, Collections.singletonList("ALL-UNNAMED"));
             }
         }
         return this;
