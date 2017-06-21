2398fc3349a7052bf4b2359cf58979caaca14393
Stuart Monteith <stuart.monteith@linaro.org>
2017-06-21 10:00:53 -0700

    7901994: jtreg needs to be updated for JT Harness 5.0

--- src/share/classes/com/sun/javatest/regtest/tool/Help.java
+++ src/share/classes/com/sun/javatest/regtest/tool/Help.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -329,7 +329,7 @@ public class Help {
         try{
             Field f = HelpTree.class.getDeclaredField("nodeComparator");
             f.setAccessible(true);
-            f.set(null, new NodeComparator());
+            f.set(commandHelpTree, new NodeComparator());
         } catch (IllegalAccessException | IllegalArgumentException | NoSuchFieldException | SecurityException ignore) {
         }
 
