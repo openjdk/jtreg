237512ffda11ee0798caa20ba403f9dab97bed85
Stefan Karlsson <stefank@openjdk.org>
2021-01-28 13:29:56 +0100

    7902822: Elapsed time of MainAction is including serialization wait time

--- src/share/classes/com/sun/javatest/regtest/exec/MainAction.java
+++ src/share/classes/com/sun/javatest/regtest/exec/MainAction.java
@@ -333,13 +333,16 @@ public class MainAction extends Action
         if (nativeCode && script.getNativeDir() == null)
             return error(MAIN_NO_NATIVES);
 
-        startAction(true);
-
         if (script.isCheck()) {
+            startAction(true);
             status = passed(CHECK_PASS);
         } else {
             Lock lock = script.getLockIfRequired();
             if (lock != null) lock.lock();
+
+            // Start action after the lock is taken to ensure correct "elapsed time".
+            startAction(true);
+
             try {
                 switch (!othervmOverrideReasons.isEmpty() ? ExecMode.OTHERVM : script.getExecMode()) {
                     case AGENTVM:
