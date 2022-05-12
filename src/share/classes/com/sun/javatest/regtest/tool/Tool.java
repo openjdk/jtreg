fd5e0608e0e172c5191a2c841f48616e06ade1e5
Jonathan Gibbons <jonathan.gibbons@oracle.com>
2022-05-12 14:09:51 -0700

    CODETOOLS-7903184: Update OS version check

--- src/share/classes/com/sun/javatest/regtest/tool/Tool.java
+++ src/share/classes/com/sun/javatest/regtest/tool/Tool.java
@@ -213,7 +213,7 @@ public class Tool {
 
     private static void checkJavaOSVersion(String expectVersion) {
         String osVersion = System.getProperty("os.version");
-        if (!osVersion.equals(expectVersion)) {
+        if (!osVersion.startsWith(expectVersion)) {
             System.err.println("The version of JDK you are using to run jtreg does not report the OS version correctly.");
             System.err.println("    java.home:    " + System.getProperty("java.home"));
             System.err.println("    java.version: " + System.getProperty("java.version"));
