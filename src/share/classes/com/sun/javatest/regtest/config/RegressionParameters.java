8e74d559375769f9c6e4c82970f7207173c547f8
SendaoYan <syan@openjdk.org>
2024-11-22 04:13:37 +0000

    7903872: Option testThreadFactoryPath support relativepath
    Reviewed-by: cstein, jpai

--- src/share/classes/com/sun/javatest/regtest/config/RegressionParameters.java
+++ src/share/classes/com/sun/javatest/regtest/config/RegressionParameters.java
@@ -1288,7 +1288,7 @@ public final class RegressionParameters
     private String testThreadFactory;
 
     public void setTestThreadFactoryPath(String testThreadFactoryPath) {
-        this.testThreadFactoryPath = testThreadFactoryPath;
+        this.testThreadFactoryPath = Path.of(testThreadFactoryPath).toAbsolutePath().toString();
     }
 
     public String getTestThreadFactoryPath() {
