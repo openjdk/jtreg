a66ceee22b6368952a5e7f94cf64f5f96c581ea1
Oleksii Sylichenko <a.silichenko@gmail.com>
2025-03-05 16:50:51 +0200

    JTRegConfigurationProducer.java: - Fix logic in `isConfigurationFromContext` for checking if two configurations are identical: -- Replace the use of the current PSI element for queries with the exact element intended for execution. - Compare string-typed configuration parameters using a "not-nullify" approach (`null == ""`). - Implement `findExactRunElement` to locate the specific PSI element for execution among the current element’s parents. - Add a JUnit check to the `shouldReplace` method.

--- plugins/idea/src/main/java/com/oracle/plugin/jtreg/configuration/producers/JTRegClassConfigurationProducer.java
+++ plugins/idea/src/main/java/com/oracle/plugin/jtreg/configuration/producers/JTRegClassConfigurationProducer.java
@@ -27,37 +27,59 @@ package com.oracle.plugin.jtreg.configuration.producers;
 
 import com.intellij.execution.Location;
 import com.intellij.execution.actions.ConfigurationContext;
+import com.intellij.execution.application.ApplicationConfigurationType;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.util.Ref;
 import com.intellij.psi.*;
 import com.oracle.plugin.jtreg.configuration.JTRegConfiguration;
 import com.oracle.plugin.jtreg.service.JTRegService;
 import com.oracle.plugin.jtreg.util.JTRegUtils;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * This class generates a jtreg configuration from a given file selected in the IDE.
  */
 public class JTRegClassConfigurationProducer extends JTRegConfigurationProducer {
 
+    /**
+     * @see JTRegConfigurationProducer#isConfigurationFromContext
+     * @see com.intellij.execution.application.AbstractApplicationConfigurationProducer#setupConfigurationFromContext
+     * @see com.intellij.execution.testframework.AbstractInClassConfigurationProducer#setupConfigurationFromContext
+     * @see com.intellij.psi.util.PsiTreeUtil#isAncestor
+     */
     @Override
-    protected boolean setupConfigurationFromContext(JTRegConfiguration configuration, ConfigurationContext context, Ref<PsiElement> sourceElement) {
+    protected boolean setupConfigurationFromContext(@NotNull JTRegConfiguration configuration,
+                                                    @NotNull ConfigurationContext context,
+                                                    @NotNull Ref<PsiElement> sourceElement) {
         final Location<PsiElement> contextLocation = context.getLocation();
         assert contextLocation != null;
-        final PsiElement element = contextLocation.getPsiElement();
 
-        PsiFile psiFile = contextLocation.getPsiElement().getContainingFile();
-        if (psiFile == null ||
-                !JTRegUtils.isInJTRegRoot(psiFile.getContainingDirectory()) ||
-                (!JTRegUtils.isJTRegTestData(psiFile) &&
-                 !JTRegUtils.isTestNGTestData(psiFile))) return false;
+        PsiElement element = contextLocation.getPsiElement();
+
+        if (!JTRegUtils.isRunnableByJTReg(element)) {
+            return false;
+        }
+
         setupConfigurationModule(context, configuration);
         final Module originalModule = configuration.getConfigurationModule().getModule();
+
         configuration.setAlternativeJrePathEnabled(JTRegService.getInstance(configuration.getProject()).isAlternativeJrePathEnabled());
         configuration.setAlternativeJrePath(JTRegService.getInstance(configuration.getProject()).getAlternativeJrePath());
         configuration.setProgramParameters(JTRegService.getInstance(configuration.getProject()).getJTregOptions());
         configuration.setWorkingDirectory(JTRegService.getInstance(configuration.getProject()).getWorkDir());
-        configuration.setRunClass(psiFile.getVirtualFile().getPath());
+
+        if (element instanceof PsiDirectory runDir) {
+            configuration.setPackage(runDir.getVirtualFile().getPath());
+        } else {
+            final PsiFile runFile = (element instanceof PsiFile psiFile) ? psiFile : element.getContainingFile();
+            configuration.setRunClass(runFile.getVirtualFile().getPath());
+        }
+
         configuration.restoreOriginalModule(originalModule);
+
+        preventRunPriorityLoss(element, sourceElement);
+
+        element = findExactRunElement(element);
 
         configuration.setQuery(getQuery(element));
         configuration.setName(nameForElement(element));
@@ -66,6 +88,49 @@ public class JTRegClassConfigurationProducer extends JTRegConfigurationProducer 
         return true;
     }
 
+    /**
+     * Ensures that the Application, TestNG, or JUnit run configuration does not receive a higher priority.
+     * This applies when the user attempts to run a file containing a single class for TestNG/JUnit,
+     * or a "main" method for the Application run configuration.
+     * <p>
+     * The class {@link com.intellij.execution.actions.PreferredProducerFind} sorts the applicable runners using
+     * {@link com.intellij.execution.actions.ConfigurationFromContext#COMPARATOR},
+     * removing more general ones and retaining more specific or equal configurations.
+     * <p>
+     * When the user tries to run a test on a file, and another type of Run Configuration can intercept the execution,
+     * this method sets the {@code PsiClass} element in the {@code sourceElement} reference to ensure priority is retained.
+     *
+     * @param element       current PSI element
+     * @param sourceElement a reference to the source element for the run configuration
+     *                      (by default contains the element at caret,
+     *                      can be updated by the producer to point to a higher-level element in the tree).
+     * @see com.intellij.execution.application.AbstractApplicationConfigurationProducer#setupConfigurationFromContext
+     * @see com.intellij.execution.testframework.AbstractInClassConfigurationProducer#setupConfigurationFromContext
+     * @see com.intellij.execution.actions.PreferredProducerFind#doGetConfigurationsFromContext
+     * @see com.intellij.execution.actions.ConfigurationFromContext#COMPARATOR
+     * @see com.intellij.psi.util.PsiTreeUtil#isAncestor
+     */
+    private void preventRunPriorityLoss(PsiElement element, @NotNull Ref<PsiElement> sourceElement) {
+        if (element instanceof PsiClassOwner psiClassOwner) {
+            final PsiClass[] psiClasses = psiClassOwner.getClasses();
+            if (1 == psiClasses.length) {
+                sourceElement.set(psiClasses[0]); // for Application/TestNG/JUnit
+            } else {
+                final PsiClass mainClass = ApplicationConfigurationType.getMainClass(element);
+                if (null != mainClass) {
+                    sourceElement.set(mainClass); // for Application
+                }
+            }
+        }
+    }
+
+    /**
+     * Generates a name for the run test configuration.
+     *
+     * @param element the current PSI element.
+     * @return One of the following: {"ClassName", "ClassName::TestMethodName"}, or the default: "FileName.java".
+     * @see #findExactRunElement(PsiElement)
+     */
     private static String nameForElement(PsiElement element) {
         if (element instanceof PsiIdentifier
                 && element.getParent() instanceof PsiMethod method) {
