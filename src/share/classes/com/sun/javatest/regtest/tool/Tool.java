142783382e8b29c6b51a4bf91a435c6b73cf1198
Ashay Rane <253344819+raneashay@users.noreply.github.com>
2026-07-30 10:59:55 -0500

    Fix copyright year and doc comment

--- src/share/classes/com/sun/javatest/regtest/tool/Tool.java
+++ src/share/classes/com/sun/javatest/regtest/tool/Tool.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -2338,8 +2338,8 @@ public class Tool {
     }
 
     /**
-     * Returns whether MSys is available, by examining whether the `MSYSTEM`
-     * environment variable is non-empty.
+     * Returns whether MSys is available, by examining whether the
+     * {@code MSYSTEM} environment variable is non-empty.
      */
     private boolean isMSysDetected() {
         if (isWindows()) {
